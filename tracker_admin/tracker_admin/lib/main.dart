f45a32bbf21f3179c3b7ea434126a3f1f45d79bb
oneebkhan <khanoneeb1997@hotmail.com>
2021-03-05 20:34:16 +0500

    admin app

---
+++ tracker_admin/tracker_admin/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Splash());
+}
+
+class Splash extends StatefulWidget {
+  Splash({Key key}) : super(key: key);
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
